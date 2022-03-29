2354f55e130ab6c91bba91713c12d4c09d16b93d
ZhiHao LI <2514451089@qq.com>
2022-03-29 12:38:29 +0800

    Update connect2ubuntu.py

--- src/rotools/optitrack/win_optitrack_service/connect2ubuntu.py
+++ src/rotools/optitrack/win_optitrack_service/connect2ubuntu.py
@@ -153,11 +153,10 @@ if __name__ == "__main__":
 
     import socket
 
-    # 创建一个socket套接字，该套接字还没有建立连接
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # 绑定监听端口，这里必须填本机的IP192.168.27.238，localhost和127.0.0.1是本机之间的进程通信使用的
+    # Must be the wlan local IP192.168.13.118，localhost和127.0.0.1 is for optitrack loopback
     server.bind(('192.168.13.118', 6688))
-    # 开始监听，并设置最大连接数
+    # start to listen
     server.listen(5)
     connect, (host, port) = server.accept()
     print(u'the client %s:%s has connected.' % (host, port))
