c24d02e9e9c99959d21258656f04a6dafe9b3468
dzp <zhipengdongneu@gmail.com>
2022-04-06 12:48:08 +0800

    try fix python module import issue

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
 d['packages'] = ['rotools',
-                 'rotools.cnn', 'rotools.cnn.vae',
+                 'rotools.hpp', 'rotools.hpp.core', 'rotools.hpp.manipulation',
                  'rotools.moveit', 'rotools.moveit.core',
                  'rotools.policy', 'rotools.policy.rmp_flow',
                  'rotools.robot', 'rotools.robot.serial',
-                 'rotools.simulation',
+                 'rotools.sensing', 'rotools.sensing.core',
+                 'rotools.simulation', 'rotools.simulation.mujoco',
+                 'rotools.snapshot', 'rotools.snapshot.core',
                  'rotools.utility',
+                 'rotools.web', 'rotools.web.core',
+                 'rotools.xsens', 'rotools.xsens.core',
                  ]
 d['package_dir'] = {'': 'src'}
 # d['scripts'] = ['scripts/test_moveit.py']
