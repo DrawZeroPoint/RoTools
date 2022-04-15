4ae7a494f4dab9fbb87ca7d898ea50fe4c94b07f
Zhipeng Dong <zhipengdongneu@gmail.com>
2022-04-15 09:04:11 +0800

    add a handy script for adding jetbrains tools to path

--- misc/setup_jetbrains.sh
+++ misc/setup_jetbrains.sh
@@ -2,6 +2,23 @@
 
 APP_PATH=$HOME/.local/share/JetBrains/Toolbox/apps
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+LIGHT_GREEN='\033[1;32m'
+NC='\033[0m' # No Color
+
+echo_success() {
+  echo -e $LIGHT_GREEN$1$NC
+}
+
+echo_warning() {
+  echo -e $YELLOW$1$NC
+}
+
+echo_failure() {
+  echo -e $RED$1$NC
+}
+
 select_highest_version() {
 
   # The regex starts with ^ and end with $. In the middle, [0-9]{3} is the major version with 3 numbers,
@@ -13,9 +30,14 @@ select_highest_version() {
   MINOR_VERSION=0
   SELECTED_VERSION=0.0.0
 
+  if [ ! -d "$APP_PATH/$1/ch-0" ]; then
+    echo_warning "$1 has not been installed. Install it with JetBrains Toolbox and then come back"
+    return
+  fi
+
   for dir in $APP_PATH/$1/ch-0/*.*.*; do
     if [[ $(basename "$dir") =~ $kREGEX_VERSION ]]; then
-      echo "Found $1 version: $(basename "$dir")"
+      echo_success "Found $1 version: $(basename "$dir")"
 
       LOCAL_MAJOR=$(echo $(basename "$dir") | cut -d'.' -f 1)
       LOCAL_MIDDLE=$(echo $(basename "$dir") | cut -d'.' -f 2)
@@ -28,17 +50,17 @@ select_highest_version() {
   done
 
   if [[ ":$PATH:" == *":$APP_PATH/$1/ch-0/$SELECTED_VERSION/bin:"* ]]; then
-    echo "$1's bin folder has already been added to PATH"
+    echo_success "$1's bin folder has already been added to PATH"
   else
     echo "export PATH=\$JB_PATH/$1/ch-0/$SELECTED_VERSION/bin:\$PATH" >>~/.bashrc
-    echo "Added the bin folder of $1 $SELECTED_VERSION to PATH"
+    echo_success "Added the bin folder of $1 $SELECTED_VERSION to PATH"
   fi
 }
 
 if [ -d "$APP_PATH" ]; then
-  echo "Found JetBrains Toolbox"
+  echo_success "Found JetBrains Toolbox"
 else
-  echo "JetBrains Toolbox not found in the default location:
+  echo_warning "JetBrains Toolbox not found in the default location:
   $APP_PATH,
   you may need to install it and rerun this script."
   exit 0
@@ -46,9 +68,9 @@ fi
 
 if [ -z ${JB_PATH+x} ]; then
   echo "export JB_PATH=$APP_PATH" >>~/.bashrc
-  echo "Added JB_PATH $APP_PATH to bashrc"
+  echo_success "Added JB_PATH $APP_PATH to bashrc"
 else
-  echo "JB_PATH has been set to '$JB_PATH'"
+  echo_success "JB_PATH has been set to '$JB_PATH'"
 fi
 
 select_highest_version "CLion"
