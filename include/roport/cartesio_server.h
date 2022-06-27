39267bc3a5ad15519fd03932ab5327a7f0de38cc
Zhipeng Dong <zhipengdongneu@gmail.com>
2022-06-27 20:36:55 +0800

    improve

--- include/roport/cartesio_server.h
+++ include/roport/cartesio_server.h
@@ -8,8 +8,8 @@
 
 #include <cartesian_interface/ReachPoseActionGoal.h>
 
-#include <actionlib/server/simple_action_server.h>
 #include <actionlib/client/simple_action_client.h>
+#include <actionlib/server/simple_action_server.h>
 
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
@@ -31,10 +31,13 @@ class CartesIOServer {
  private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
+
   std::vector<std::string> group_names_;
-  std::string reference_frame_;
-//  std::vector<std::shared_ptr<MoveGroupInterface>> interfaces_;
+  std::vector<std::string> controlled_frames_;
+  std::vector<std::string> reference_frames_;
+
   tf2_ros::Buffer tf_buffer_;
+  tf2_ros::TransformListener tf_listener_;
 
   ros::ServiceServer execute_all_poses_srv_;
   ros::ServiceServer execute_mirrored_pose_srv_;
@@ -42,54 +45,38 @@ class CartesIOServer {
   using reachPoseActionClient = actionlib::SimpleActionClient<cartesian_interface::ReachPoseAction>;
   std::vector<std::shared_ptr<reachPoseActionClient>> control_clients_;
 
-//  auto executeAllPosesSrvCb(roport::ExecuteAllPoses::Request& req, roport::ExecuteAllPoses::Response& resp) -> bool;
-//  auto executeMirroredPoseSrvCb(roport::ExecuteMirroredPose::Request& req, roport::ExecuteMirroredPose::Response& resp)
-//      -> bool;
-//
-//  /**
-//   *
-//   * @param move_group The move group to build the trajectory for
-//   * @param pose Target pose of the trajectory
-//   * @param trajectory Output trajectory
-//   * @param do_cartesian
-//   * @return
-//   */
-//  static auto buildTrajectory(MoveGroupInterface& move_group,
-//                              const geometry_msgs::PoseStamped& pose,
-//                              trajectory_msgs::JointTrajectory& trajectory,
-//                              bool do_cartesian = false) -> bool;
-//
-//  static auto buildTrajectory(const std::shared_ptr<MoveGroupInterface>& move_group,
-//                              const std::vector<geometry_msgs::PoseStamped>& poses,
-//                              const std::vector<ros::Duration>& time_stamps,
-//                              trajectory_msgs::JointTrajectory& trajectory) -> bool;
-//
-//  static void updateTrajectoryStamp(trajectory_msgs::JointTrajectory traj_in,
-//                                    double stamp,
-//                                    trajectory_msgs::JointTrajectory& traj_out);
-//
-//  auto executeTrajectories(const std::map<int, trajectory_msgs::JointTrajectory>& trajectories, double duration = 120)
-//      -> bool;
-//
-//  void buildControllerGoal(int group_id,
-//                           const trajectory_msgs::JointTrajectory& trajectory,
-//                           control_msgs::FollowJointTrajectoryGoal& goal);
-//
-//  auto buildTolerance(int group_id, double position, double velocity, double acceleration)
-//      -> std::vector<control_msgs::JointTolerance>;
-//
-//  void relativePoseToAbsolutePose(const geometry_msgs::PoseStamped& transform_wrt_local_base,
-//                                  const geometry_msgs::PoseStamped& current_pose_wrt_base,
-//                                  geometry_msgs::PoseStamped& goal_pose_wrt_base);
-//  static void getMirroredTrajectory(MoveGroupInterface& move_group,
-//                                    trajectory_msgs::JointTrajectory trajectory,
-//                                    std::vector<double> mirror_vector,
-//                                    trajectory_msgs::JointTrajectory& mirrored_trajectory);
-//
-//  static void geometryPoseToEigenMatrix(const geometry_msgs::PoseStamped& pose, Eigen::Matrix4d& mat);
-//  static void eigenMatrixToGeometryPose(Eigen::Matrix4d mat, geometry_msgs::PoseStamped& pose);
+  auto executeAllPosesSrvCb(roport::ExecuteAllPoses::Request& req, roport::ExecuteAllPoses::Response& resp) -> bool;
+  //  auto executeMirroredPoseSrvCb(roport::ExecuteMirroredPose::Request& req, roport::ExecuteMirroredPose::Response&
+  //  resp)
+  //      -> bool;
+  //
 
-  auto getGroupIndex(const std::string& group_name) -> int;
+  void buildActionGoal(const int& index,
+                       const geometry_msgs::Pose& goal_pose,
+                       cartesian_interface::ReachPoseActionGoal& action_goal);
+  //
+  //  static auto buildTrajectory(const std::shared_ptr<MoveGroupInterface>& move_group,
+  //                              const std::vector<geometry_msgs::PoseStamped>& poses,
+  //                              const std::vector<ros::Duration>& time_stamps,
+  //                              trajectory_msgs::JointTrajectory& trajectory) -> bool;
+
+  static void updateStamp(const double& stamp, cartesian_interface::ReachPoseActionGoal& action_goal);
+
+  auto executeGoals(const std::vector<cartesian_interface::ReachPoseActionGoal>& goals, double duration = 120) -> bool;
+
+  //  void buildControllerGoal(int group_id,
+  //                           const trajectory_msgs::JointTrajectory& trajectory,
+  //                           control_msgs::FollowJointTrajectoryGoal& goal);
+  //
+  //  auto buildTolerance(int group_id, double position, double velocity, double acceleration)
+  //      -> std::vector<control_msgs::JointTolerance>;
+  //
+  //  static void getMirroredTrajectory(MoveGroupInterface& move_group,
+  //                                    trajectory_msgs::JointTrajectory trajectory,
+  //                                    std::vector<double> mirror_vector,
+  //                                    trajectory_msgs::JointTrajectory& mirrored_trajectory);
+
+  bool getTransform(const int& index, geometry_msgs::TransformStamped& transform);
 };
 
 }  // namespace roport
--- src/lib/cartesio_server.cpp
+++ src/lib/cartesio_server.cpp
@@ -4,37 +4,49 @@
 
 #include "roport/cartesio_server.h"
 
-#include <utility>
+#include "roport/common.h"
 
 namespace roport {
 
 CartesIOServer::CartesIOServer(const ros::NodeHandle& node_handle, const ros::NodeHandle& pnh)
-    : nh_(node_handle), pnh_(pnh) {
+    : nh_(node_handle), pnh_(pnh), tf_listener_(tf_buffer_) {
   // Get all available planning group names
   XmlRpc::XmlRpcValue group_names;
-  pnh_.getParam("group_names", group_names);
+  getParam(nh_, pnh_, "group_names", group_names);
   ROS_ASSERT(group_names.getType() == XmlRpc::XmlRpcValue::TypeArray);
 
   if (group_names.size() == 0) {
-    throw std::runtime_error("RoPort: Move group name list is None");
+    throw std::runtime_error("No group name");
   }
   for (int i = 0; i < group_names.size(); i++) {
     ROS_ASSERT(group_names[i].getType() == XmlRpc::XmlRpcValue::TypeString);
     group_names_.push_back(group_names[i]);
   }
 
-  // Get global reference frame for all groups
-  pnh_.getParam("reference_frame", reference_frame_);
+  XmlRpc::XmlRpcValue controlled_frames;
+  getParam(nh_, pnh_, "controlled_frames", controlled_frames);
+  ROS_ASSERT(controlled_frames.getType() == XmlRpc::XmlRpcValue::TypeArray);
 
-  // Initialize planning groups
-  //  for (auto& group_name : group_names_) {
-  //    ROS_INFO_STREAM("RoPort: Initializing move group interface " << group_name);
-  //    auto group = std::make_shared<MoveGroupInterface>(group_name);
-  //    if (!reference_frame.empty()) {
-  //      group->setPoseReferenceFrame(reference_frame);
-  //    }
-  //    interfaces_.push_back(group);
-  //  }
+  if (controlled_frames.size() != group_names_.size()) {
+    throw std::runtime_error("Controlled frames size does not match group names");
+  }
+  for (int i = 0; i < controlled_frames.size(); i++) {
+    ROS_ASSERT(controlled_frames[i].getType() == XmlRpc::XmlRpcValue::TypeString);
+    controlled_frames_.push_back(controlled_frames[i]);
+  }
+
+  XmlRpc::XmlRpcValue reference_frames;
+  getParam(nh_, pnh_, "reference_frames", reference_frames);
+  ROS_ASSERT(reference_frames.getType() == XmlRpc::XmlRpcValue::TypeArray);
+
+  if (reference_frames.size() != controlled_frames_.size()) {
+    throw std::runtime_error("Reference frames size does not match controlled frames");
+  }
+  for (int i = 0; i < reference_frames.size(); i++) {
+    ROS_ASSERT(reference_frames[i].getType() == XmlRpc::XmlRpcValue::TypeString);
+    reference_frames_.push_back(reference_frames[i]);
+    ROS_INFO_STREAM("listening the transform " << reference_frames_[i] << " -> " << controlled_frames_[i]);
+  }
 
   // Initialize controller action clients, one for each group
   const double kTimeout = 3.;
@@ -49,61 +61,77 @@ CartesIOServer::CartesIOServer(const ros::NodeHandle& node_handle, const ros::No
   }
 
   // Initialize control servers
-//  execute_all_poses_srv_ = nh_.advertiseService("execute_all_poses", &CartesIOServer::executeAllPosesSrvCb, this);
+  execute_all_poses_srv_ = nh_.advertiseService("execute_all_poses", &CartesIOServer::executeAllPosesSrvCb, this);
   //  execute_mirrored_pose_srv_ =
   //      nh_.advertiseService("execute_mirrored_pose", &CartesIOServer::executeMirroredPoseSrvCb, this);
 }
 
-//auto CartesIOServer::executeAllPosesSrvCb(roport::ExecuteAllPoses::Request& req,
-//                                          roport::ExecuteAllPoses::Response& resp) -> bool {
-//  ROS_ASSERT(req.group_names.size() == req.goals.poses.size());
-//  std::map<int, trajectory_msgs::JointTrajectory> trajectories;
-//
-//  for (int i = 0; i < group_names_.size(); ++i) {
-//    for (int j = 0; j < req.group_names.size(); ++j) {
-//      if (req.group_names[j] == group_names_[i]) {
-//        trajectory_msgs::JointTrajectory trajectory;
-//        geometry_msgs::PoseStamped goal_pose_wrt_base;
-//        goal_pose_wrt_base.header.frame_id = reference_frame_;
-//
-//        // Transfer given pose to base frame
-//        if (req.goal_type == req.BASE_ABS) {
-//          goal_pose_wrt_base.pose = req.goals.poses[j];
-//        } else if (req.goal_type == req.BASE_REL) {
-//          geometry_msgs::PoseStamped transform_wrt_local_base;
-//          transform_wrt_local_base.pose = req.goals.poses[j];
-//          relativePoseToAbsolutePose(transform_wrt_local_base, current_pose_wrt_base, goal_pose_wrt_base);
-//        } else {
-//          geometry_msgs::PoseStamped goal_pose_wrt_eef;
-//          goal_pose_wrt_eef.pose = req.goals.poses[j];
-//          goal_pose_wrt_eef.header.frame_id = interfaces_[i]->getEndEffectorLink();
-//          goal_pose_wrt_base =
-//              tf_buffer_.transform(goal_pose_wrt_eef, interfaces_[i]->getPlanningFrame(), ros::Duration(1));
-//        }
-//
-//        // Build trajectory to reach the goal
-//        if (!buildTrajectory(*interfaces_[i], goal_pose_wrt_base, trajectory)) {
-//          ROS_ERROR("Building trajectory failed");
-//          resp.result_status = resp.FAILED;
-//          return true;
-//        }
-//        if (req.stamps.size() == req.group_names.size() && req.stamps[j] > 0) {
-//          trajectory_msgs::JointTrajectory updated_trajectory;
-//          updateTrajectoryStamp(trajectory, req.stamps[j], updated_trajectory);
-//          trajectories.insert({i, updated_trajectory});
-//        } else {
-//          trajectories.insert({i, trajectory});
-//        }
-//      }
-//    }
-//  }
-//  if (executeTrajectories(trajectories)) {
-//    resp.result_status = resp.SUCCEEDED;
-//  } else {
-//    resp.result_status = resp.FAILED;
-//  }
-//  return true;
-//}
+auto CartesIOServer::executeAllPosesSrvCb(roport::ExecuteAllPoses::Request& req,
+                                          roport::ExecuteAllPoses::Response& resp) -> bool {
+  ROS_ASSERT(req.group_names.size() == req.goals.poses.size());
+  std::vector<cartesian_interface::ReachPoseActionGoal> action_goals;
+
+  for (int i = 0; i < group_names_.size(); ++i) {
+    for (int j = 0; j < req.group_names.size(); ++j) {
+      if (req.group_names[j] == group_names_[i]) {
+        geometry_msgs::TransformStamped current_transform;
+        if (!getTransform(i, current_transform)) {
+          return false;
+        }
+
+        geometry_msgs::Pose current_pose;
+        current_pose.position.x = current_transform.transform.translation.x;
+        current_pose.position.y = current_transform.transform.translation.y;
+        current_pose.position.z = current_transform.transform.translation.z;
+        current_pose.orientation.x = current_transform.transform.rotation.x;
+        current_pose.orientation.y = current_transform.transform.rotation.y;
+        current_pose.orientation.z = current_transform.transform.rotation.z;
+        current_pose.orientation.w = current_transform.transform.rotation.w;
+
+        // Initialize the goal pose
+        geometry_msgs::Pose goal_pose;
+
+        // Transfer the given pose to the reference frame
+        if (req.goal_type == req.BASE_ABS) {
+          // The given pose is already in the reference frame
+          goal_pose = req.goals.poses[j];
+        } else if (req.goal_type == req.BASE_REL) {
+          // The given pose is relative to the local aligned frame having the same orientation as the reference frame
+          localAlignedPoseToGlobalPose(req.goals.poses[j], current_pose, goal_pose);
+        } else {
+          // The given pose is relative to the local frame
+          localPoseToGlobalPose(req.goals.poses[j], current_pose, goal_pose);
+        }
+
+        // Build trajectory to reach the goal
+        cartesian_interface::ReachPoseActionGoal action_goal;
+        buildActionGoal(i, goal_pose, action_goal);
+        if (req.stamps.size() == req.group_names.size() && req.stamps[j] > 0) {
+          updateStamp(req.stamps[j], action_goal);
+          action_goals.push_back(action_goal);
+        } else {
+          action_goals.push_back(action_goal);
+        }
+      }
+    }
+  }
+  if (executeGoals(action_goals)) {
+    resp.result_status = resp.SUCCEEDED;
+  } else {
+    resp.result_status = resp.FAILED;
+  }
+  return true;
+}
+
+bool CartesIOServer::getTransform(const int& index, geometry_msgs::TransformStamped& transform) {
+  try {
+    transform = tf_buffer_.lookupTransform(controlled_frames_[index], reference_frames_[index], ros::Time(0));
+    return true;
+  } catch (tf2::TransformException& ex) {
+    ROS_WARN("%s", ex.what());
+    return false;
+  }
+}
 
 // auto CartesIOServer::executeMirroredPoseSrvCb(roport::ExecuteMirroredPose::Request& req,
 //                                               roport::ExecuteMirroredPose::Response& resp) -> bool {
@@ -164,26 +192,6 @@ CartesIOServer::CartesIOServer(const ros::NodeHandle& node_handle, const ros::No
 //   return true;
 // }
 //
-// void CartesIOServer::relativePoseToAbsolutePose(const geometry_msgs::PoseStamped& transform_wrt_local_base,
-//                                                 const geometry_msgs::PoseStamped& current_pose_wrt_base,
-//                                                 geometry_msgs::PoseStamped& goal_pose_wrt_base) {
-//   Eigen::Matrix4d mat_be;
-//   geometryPoseToEigenMatrix(current_pose_wrt_base, mat_be);
-//   // mat_bl
-//   Eigen::Matrix4d mat_bl = Eigen::Matrix4d::Identity();
-//   Eigen::Vector3d t_bl(current_pose_wrt_base.pose.position.x, current_pose_wrt_base.pose.position.y,
-//                        current_pose_wrt_base.pose.position.z);
-//   mat_bl.topRightCorner(3, 1) = t_bl;
-//   // mat_le
-//   Eigen::Matrix4d mat_le = mat_bl.inverse() * mat_be;
-//   // mat_ll_new
-//   Eigen::Matrix4d mat_ll_new;
-//   geometryPoseToEigenMatrix(transform_wrt_local_base, mat_ll_new);
-//   // mat_be_new
-//   Eigen::Matrix4d mat_be_new = mat_bl * mat_ll_new * mat_le;
-//   eigenMatrixToGeometryPose(mat_be_new, goal_pose_wrt_base);
-// }
-//
 // void CartesIOServer::getMirroredTrajectory(MoveGroupInterface& move_group,
 //                                            trajectory_msgs::JointTrajectory trajectory,
 //                                            std::vector<double> mirror_vector,
@@ -209,129 +217,37 @@ CartesIOServer::CartesIOServer(const ros::NodeHandle& node_handle, const ros::No
 //     mirrored_trajectory.points.push_back(p);
 //   }
 // }
-//
-// auto CartesIOServer::buildTrajectory(MoveGroupInterface& move_group,
-//                                      const geometry_msgs::PoseStamped& pose,
-//                                      trajectory_msgs::JointTrajectory& trajectory,
-//                                      bool do_cartesian) -> bool {
-//   moveit_msgs::RobotTrajectory robot_trajectory;
-//   if (!do_cartesian) {
-//     move_group.clearPoseTargets();
-//     move_group.setPoseTarget(pose);
-//     moveit::planning_interface::MoveGroupInterface::Plan plan;
-//     auto res = move_group.plan(plan);
-//     if (res != moveit::planning_interface::MoveItErrorCode::SUCCESS) {  // NOLINT
-//       ROS_ERROR_STREAM("RoPort: Planning for group " + move_group.getName() + " failed with MoveItErrorCode " +
-//                        std::to_string(res.val));
-//       return false;
-//     }
-//     robot_trajectory = plan.trajectory_;
-//   } else {
-//     const double kJumpThreshold = 0.0;  // No joint-space jump constraint (see moveit_msgs/GetCartesianPath)
-//     const double kEEStep = 0.01;
-//
-//     std::vector<geometry_msgs::Pose> way_points;
-//     way_points.push_back(pose.pose);
-//     double fraction = move_group.computeCartesianPath(way_points, kEEStep, kJumpThreshold, robot_trajectory);
-//     if (fraction != 1) {
-//       ROS_ERROR_STREAM("RoPort: Planning for group " + move_group.getName() + " failed with fraction " +
-//                        std::to_string(fraction));
-//       return false;
-//     }
-//   }
-//   trajectory = robot_trajectory.joint_trajectory;
-//   trajectory.joint_names = move_group.getActiveJoints();
-//   assert(!trajectory.joint_names.empty());
-//   return true;
-// }
-//
-// auto CartesIOServer::buildTrajectory(const std::shared_ptr<MoveGroupInterface>& move_group,
-//                                      const std::vector<geometry_msgs::PoseStamped>& poses,
-//                                      const std::vector<ros::Duration>& time_stamps,
-//                                      trajectory_msgs::JointTrajectory& trajectory) -> bool {
-//   if (poses.empty()) {
-//     throw std::invalid_argument("RoPort: Received empty pose vector");
-//   }
-//
-//   if (poses.size() != time_stamps.size()) {
-//     throw std::invalid_argument("RoPort: Poses and time_stamps should have the same size, but " +
-//                                 std::to_string(poses.size()) + " vs " + std::to_string(time_stamps.size()));
-//   }
-//
-//   moveit::core::RobotStatePtr robot_state = move_group->getCurrentState();
-//   std::string group_name = move_group->getName();
-//   const moveit::core::JointModelGroup* joint_group = move_group->getCurrentState()->getJointModelGroup(group_name);
-//
-//   std::vector<std::vector<double>> joint_poses;
-//   for (unsigned int i = 0; i < poses.size(); i++) {
-//     const geometry_msgs::Pose& pose = poses[i].pose;
-//     std::vector<double> joint_pose;
-//     if (!robot_state->setFromIK(joint_group, pose, move_group->getEndEffectorLink(), 1.0)) {
-//       throw std::runtime_error("RoPort: followEePoseTrajectory failed, IK solution not found for pose " +
-//                                std::to_string(i) + " ( requested pose is (" + std::to_string(pose.position.x) + ", "
-//                                + std::to_string(pose.position.y) + ", " + std::to_string(pose.position.z) + ") (" +
-//                                std::to_string(pose.orientation.x) + ", " + std::to_string(pose.orientation.y) + ", "
-//                                + std::to_string(pose.orientation.z) + ", " + std::to_string(pose.orientation.w) +
-//                                ") transformed from " + poses.at(i).header.frame_id + ")");
-//     }
-//     robot_state->copyJointGroupPositions(joint_group, joint_pose);
-//     joint_poses.push_back(joint_pose);
-//   }
-//
-//   trajectory.joint_names = joint_group->getActiveJointModelNames();
-//   for (unsigned int i = 0; i < joint_poses.size(); i++) {
-//     trajectory_msgs::JointTrajectoryPoint point;
-//     point.positions = joint_poses.at(i);
-//     point.time_from_start = time_stamps.at(i);
-//     trajectory.points.push_back(point);
-//   }
-//   return true;
-// }
-//
-// void CartesIOServer::updateTrajectoryStamp(trajectory_msgs::JointTrajectory traj_in,
-//                                            double stamp,
-//                                            trajectory_msgs::JointTrajectory& traj_out) {
-//   traj_out.header = traj_in.header;
-//   traj_out.joint_names = traj_in.joint_names;
-//
-//   ros::Duration last_stamp = traj_in.points[traj_in.points.size() - 1].time_from_start;
-//   double ratio = stamp / last_stamp.toSec();
-//   for (auto& traj_point : traj_in.points) {
-//     trajectory_msgs::JointTrajectoryPoint point;
-//     point.positions = traj_point.positions;
-//     point.time_from_start = ros::Duration(traj_point.time_from_start.toSec() * ratio);
-//     traj_out.points.push_back(point);
-//   }
-// }
-//
-// auto CartesIOServer::executeTrajectories(const std::map<int, trajectory_msgs::JointTrajectory>& trajectories,
-//                                          double duration) -> bool {
-//   std::vector<control_msgs::FollowJointTrajectoryGoal> goals;
-//   for (const auto& traj : trajectories) {
-//     control_msgs::FollowJointTrajectoryGoal goal;
-//     buildControllerGoal(traj.first, traj.second, goal);
-//     goals.push_back(goal);
-//   }
-//
-//   std::vector<std::shared_ptr<FJT_actionClient>> clients;
-//   for (const auto& traj : trajectories) {
-//     std::shared_ptr<FJT_actionClient> client = control_clients_[traj.first];
-//     clients.push_back(client);
-//   }
-//
-//   for (size_t i = 0; i < clients.size(); ++i) {
-//     clients[i]->sendGoal(goals[i]);
-//   }
-//
-//   for (auto& client : clients) {
-//     if (!client->waitForResult(ros::Duration(duration))) {
-//       ROS_ERROR("RoPort: Trajectory execution timeout after %f seconds", duration);
-//       return false;
-//     }
-//   }
-//   return true;
-// }
-//
+
+void CartesIOServer::buildActionGoal(const int& index,
+                                     const geometry_msgs::Pose& goal_pose,
+                                     cartesian_interface::ReachPoseActionGoal& action_goal) {
+  action_goal.header.frame_id = reference_frames_[index];
+  action_goal.goal.frames.push_back(goal_pose);
+  action_goal.goal.time.push_back(1.);
+  action_goal.goal.incremental = true;
+}
+
+void CartesIOServer::updateStamp(const double& stamp, cartesian_interface::ReachPoseActionGoal& action_goal) {
+  for (float& i : action_goal.goal.time) {
+    i = static_cast<float>(stamp);
+  }
+}
+
+auto CartesIOServer::executeGoals(const std::vector<cartesian_interface::ReachPoseActionGoal>& goals, double duration)
+    -> bool {
+  for (size_t i = 0; i < control_clients_.size(); ++i) {
+    control_clients_[i]->sendGoal(goals[i].goal);
+  }
+
+  for (size_t i = 0; i < control_clients_.size(); ++i) {
+    if (!control_clients_[i]->waitForResult(ros::Duration(duration))) {
+      ROS_ERROR("Goal %zu of group %s execution timeout after %f seconds", i, group_names_[i].c_str(), duration);
+      return false;
+    }
+  }
+  return true;
+}
+
 // void CartesIOServer::buildControllerGoal(int group_id,
 //                                          const trajectory_msgs::JointTrajectory& trajectory,
 //                                          control_msgs::FollowJointTrajectoryGoal& goal) {
@@ -349,42 +265,5 @@ CartesIOServer::CartesIOServer(const ros::NodeHandle& node_handle, const ros::No
 //   joint_tolerance.acceleration = acceleration;
 //   int joint_num = interfaces_[group_id]->getActiveJoints().size();
 //   return std::vector<control_msgs::JointTolerance>(joint_num, joint_tolerance);
-// }
-//
-// void CartesIOServer::geometryPoseToEigenMatrix(const geometry_msgs::PoseStamped& pose, Eigen::Matrix4d& mat) {
-//   mat = Eigen::Matrix4d::Identity();
-//
-//   Eigen::Quaterniond orientation;
-//   orientation.coeffs() << pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z,
-//       pose.pose.orientation.w;
-//   Eigen::Quaterniond orientation_n = orientation.normalized();
-//   Eigen::Matrix3d rotation_matrix = orientation_n.toRotationMatrix();
-//   mat.topLeftCorner(3, 3) = rotation_matrix;
-//
-//   Eigen::Vector3d t_eigen;
-//   t_eigen << pose.pose.position.x, pose.pose.position.y, pose.pose.position.z;
-//   mat.topRightCorner(3, 1) = t_eigen;
-// }
-//
-// void CartesIOServer::eigenMatrixToGeometryPose(Eigen::Matrix4d mat, geometry_msgs::PoseStamped& pose) {
-//   Eigen::Matrix3d rotation_matrix = mat.topLeftCorner(3, 3);
-//   Eigen::Vector3d t_eigen = mat.topRightCorner(3, 1);
-//   Eigen::Quaterniond quat(rotation_matrix);
-//
-//   pose.pose.position.x = t_eigen[0];
-//   pose.pose.position.y = t_eigen[1];
-//   pose.pose.position.z = t_eigen[2];
-//   pose.pose.orientation.x = quat.x();
-//   pose.pose.orientation.y = quat.y();
-//   pose.pose.orientation.z = quat.z();
-//   pose.pose.orientation.w = quat.w();
-// }
-//
-// auto CartesIOServer::getGroupIndex(const std::string& group_name) -> int {
-//   auto name = std::find(group_names_.begin(), group_names_.end(), group_name);
-//   if (name != group_names_.end()) {
-//     return name - group_names_.begin();
-//   }
-//   return -1;
 // }
 }  // namespace roport
