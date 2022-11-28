3c87c9d50adf087de78febd2ca5310fe5cc5056c
Zhipeng Dong <zhipengdongneu@gmail.com>
2022-11-28 12:35:35 +0800

    in black

--- setup.py
+++ setup.py
@@ -6,19 +6,30 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['rotools',
-                 'rotools.hpp', 'rotools.hpp.core', 'rotools.hpp.manipulation',
-                 'rotools.moveit', 'rotools.moveit.core',
-                 'rotools.policy', 'rotools.policy.rmp_flow',
-                 'rotools.robot', 'rotools.robot.serial',
-                 'rotools.sensing', 'rotools.sensing.core',
-                 'rotools.simulation', 'rotools.simulation.mujoco',
-                 'rotools.snapshot', 'rotools.snapshot.core',
-                 'rotools.utility',
-                 'rotools.websocket', 'rotools.websocket.core',
-                 'rotools.xsens', 'rotools.xsens.core',
-                 ]
-d['package_dir'] = {'': 'src'}
+d["packages"] = [
+    "rotools",
+    "rotools.hpp",
+    "rotools.hpp.core",
+    "rotools.hpp.manipulation",
+    "rotools.moveit",
+    "rotools.moveit.core",
+    "rotools.policy",
+    "rotools.policy.rmp_flow",
+    "rotools.robot",
+    "rotools.robot.serial",
+    "rotools.sensing",
+    "rotools.sensing.core",
+    "rotools.simulation",
+    "rotools.simulation.mujoco",
+    "rotools.snapshot",
+    "rotools.snapshot.core",
+    "rotools.utility",
+    "rotools.websocket",
+    "rotools.websocket.core",
+    "rotools.xsens",
+    "rotools.xsens.core",
+]
+d["package_dir"] = {"": "src"}
 # d['scripts'] = ['scripts/test_moveit.py']
 
 setup(**d)
